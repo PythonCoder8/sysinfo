e8eadbe9e516484bb24ba508cb85eaf68ab1e588
Coder <72826534+PythonCoder8@users.noreply.github.com>
2020-10-25 16:38:12 -0700

    Update sysinfo.py

--- sysinfo.py
+++ sysinfo.py
@@ -1,10 +1,24 @@
+#Sysinfo.py - Get info about computer and OS
+#This code has only been tested on Ubuntu 20.04 LTS and may not work on any other distros of Linux.
+
+#Import all required libraries/modules
 import subprocess as sp
 import platform
 import os
+
+#Ask user if they have the Linux command line tool figlet installed
 figlet_install = input("Do you have figlet installed on your linux terminal (Y/N)? ")
+
+#If they already have it installed, move on to the next part in the code
 if figlet_install.upper() == "Y":
     pass
+
+#If user does not have figlet installed, in the background the program runs the Linux command for installing figlet.
 if figlet_install.upper() == "N":
     os.system("sudo apt-get install figlet")
+    
+#Run figlet and get the name of the user's computer
 os.system("figlet %s" %(platform.node()))
-sp.call("welcomewagon.sh")
+
+#Execute bash script sysinfo.sh
+sp.call("sysinfo.sh")
